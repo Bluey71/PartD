84a17334d9df987d3c70f6a86eb95d43305919b1
Bluey71 <tinman7171@gmail.com>
2021-01-17 13:06:49 +1300

    First commit

--- BIT504 Ass 2 Part D/src/Main.java
+++ BIT504 Ass 2 Part D/src/Main.java
@@ -3,9 +3,6 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hi, my name is Scott");
-		System.out.println("My favourite colour is blue");
-		System.out.println("My favourite mode of transportation is car");
-		System.out.println("The best day of the week is thirsty Thursday");
 	}
 
 }
